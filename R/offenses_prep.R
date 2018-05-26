64cae6b3679479d7c5d7e6c9ebfa2f4ab8b22dc0
jacobkap <jkkaplan6@gmail.com>
2018-05-26 09:29:11 -0400

    adds state files for easy ajax for tool.

---
+++ R/offenses_prep.R
@@ -0,0 +1,70 @@
+load("C:/Users/user/AppData/Local/Temp/Temp1_ucr_offenses_known_1960_2016_rda.zip/ucr_offenses_known_yearly_1960_2016.rda")
+load("C:/Users/user/Dropbox/R_project/crimedatatool_helper/data/crosswalk_agencies.rda")
+library(tidyverse)
+
+simpleCap <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1,1)), substring(s, 2),
+        sep="", collapse=" ")
+}
+
+ucr <-
+  ucr_offenses_known_yearly_1960_2016 %>%
+  filter(months_reported == "december is the last month reported",
+         !state %in% c("guam", "canal zone", "puerto rico", "virgin islands")) %>%
+  left_join(crosswalk_agencies) %>%
+  mutate(population = population_1 + population_2 + population_3,
+         agency = tolower(agency)) %>%
+  modify_at(to_drop, ~NULL) %>%
+  rename(ORI = ori,
+         ORI9 = ori9) %>%
+  select(agency,
+         year,
+         state,
+         population,
+         ORI,
+         ORI9,
+         fips_state_code,
+         fips_county_code,
+         everything())
+ucr$agency <- sapply(ucr$agency, simpleCap)
+ucr$state <- sapply(ucr$state, simpleCap)
+ucr$state <- gsub(" Of ", " of ", ucr$state)
+
+
+setwd("C:/Users/user/Dropbox/R_project/crimedatatool_helper/data/offenses")
+for (state_val in unique(ucr$state)) {
+  temp <-
+    ucr %>%
+    filter(state == state_val)
+  state_val <- gsub(" ", "_", state_val)
+  write_csv(temp, path = paste0("offenses_", state_val, ".csv"))
+}
+
+
+
+to_drop <- c("state_abb",
+             "months_reported",
+             "fips_state_county_code",
+             "fips_place_code",
+             "fips_state_place_code",
+             "division",
+             "core_city_indication",
+             "covered_by_code",
+             "population_1",
+             "county_1",
+             "msa_1",
+             "population_2",
+             "county_2",
+             "msa_2",
+             "population_3",
+             "county_3",
+             "msa_3",
+             "followup_indication",
+             "special_mailing_group",
+             "special_mailing_address",
+             "mailing_address_line_1",
+             "mailing_address_line_2",
+             "mailing_address_line_3",
+             "mailing_address_line_4",
+             "zip_code")
