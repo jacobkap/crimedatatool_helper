4631a4bdcd77bdb7f7d4b5ffd0b48a23c7b2d538
jacobkap <jkkaplan6@gmail.com>
2018-12-29 21:15:11 -0500

    update arrests data.

--- R/arrests_prep.R
+++ R/arrests_prep.R
@@ -1,88 +1,108 @@
 source('C:/Users/user/Dropbox/R_project/crimedatatool_helper/R/utils.R')
+load("C:/Users/user/Dropbox/R_project/crime_data/clean_data/arrests/ucr_arrests_yearly_all_crimes_totals_race_1974_2016.rda")
+load("C:/Users/user/Dropbox/R_project/crime_data/clean_data/arrests/ucr_arrests_yearly_all_crimes_totals_sex_1974_2016.rda")
 
-load("C:/Users/user/Dropbox/R_project/crime_data/clean_data/ASR/asr_simple_crimes_sex_1980_2016.rda")
-asr_simple_crimes_sex_1980_2016 <-
-  asr_simple_crimes_sex_1980_2016 %>%
+ucr_arrests_yearly_all_crimes_totals_sex_1974_2016 <-
+  ucr_arrests_yearly_all_crimes_totals_sex_1974_2016 %>%
   dplyr::filter(number_of_months_reported %in% 12)
-load("C:/Users/user/Dropbox/R_project/crime_data/clean_data/ASR/asr_simple_crimes_race_1980_2016.rda")
-common_names <- names(asr_simple_crimes_sex_1980_2016) %in% names(asr_simple_crimes_race_1980_2016)
-common_names <- names(asr_simple_crimes_sex_1980_2016)
+gc()
+common_names <- names(ucr_arrests_yearly_all_crimes_totals_sex_1974_2016)[names(ucr_arrests_yearly_all_crimes_totals_sex_1974_2016) %in% names(ucr_arrests_yearly_all_crimes_totals_race_1974_2016)]
 common_names <- common_names[!common_names %in% c("ori", "year")]
-asr_simple_crimes_race_1980_2016 <-
-  asr_simple_crimes_race_1980_2016 %>%
+ucr_arrests_yearly_all_crimes_totals_race_1974_2016 <-
+  ucr_arrests_yearly_all_crimes_totals_race_1974_2016 %>%
   dplyr::filter(number_of_months_reported %in% 12) %>%
   dplyr::select(-one_of(common_names))
-
+gc()
 arrests <-
-  asr_simple_crimes_sex_1980_2016 %>%
-  dplyr::left_join(asr_simple_crimes_race_1980_2016) %>%
+  ucr_arrests_yearly_all_crimes_totals_sex_1974_2016 %>%
+  dplyr::left_join(ucr_arrests_yearly_all_crimes_totals_race_1974_2016) %>%
   dplyr::filter(!state %in% c("guam",
                               "canal zone",
                               "puerto rico",
-                              "virgin islands"),
-                number_of_months_reported %in% 12) %>%
+                              "virgin islands")) %>%
   dplyr::left_join(crosswalk_agencies) %>%
   dplyr::filter(agency != "NANA") %>%
   dplyr::select(-one_of(arrests_to_drop)) %>%
   dplyr::rename(ORI               = ori) %>%
   dplyr::select(starting_cols,
+                dplyr::matches("tot_adult"),
+                dplyr::matches("tot_juv"),
+                dplyr::matches("tot_arrests"),
+                dplyr::matches("tot_(fe)?male_adult$"),
+                dplyr::matches("tot_(fe)?male_juv$"),
+                dplyr::matches("tot_(fe)?male$"),
+                dplyr::matches("adult_(asian|amer|black|white)"),
+                dplyr::matches("juv_(asian|amer|black|white)"),
+                dplyr::matches("tot_(asian|amer|black|white)"),
+                everything()) %>%
+  dplyr::select(starting_cols,
                 dplyr::matches("agg_assault"),
+                dplyr::matches("all_other"),
                 dplyr::matches("oth_assault"),
                 dplyr::matches("arson"),
                 dplyr::matches("burglary"),
-                dplyr::matches("curfew"),
+                dplyr::matches("curfew_loiter"),
                 dplyr::matches("disorder_cond"),
 
                 dplyr::matches("total_drug"),
-                dplyr::matches("poss_drug"),
+                dplyr::matches("poss_drug_total"),
                 dplyr::matches("poss_synth_narc"),
                 dplyr::matches("poss_cannabis"),
                 dplyr::matches("poss_heroin_coke"),
-                dplyr::matches("poss_oth_drug"),
-                dplyr::matches("sale_drug"),
+                dplyr::matches("poss_other_drug"),
+                dplyr::matches("sale_drug_total"),
                 dplyr::matches("sale_synth_narc"),
                 dplyr::matches("sale_cannabis"),
                 dplyr::matches("sale_heroin_coke"),
-                dplyr::matches("sale_oth_drug"),
+                dplyr::matches("sale_other_drug"),
 
                 dplyr::matches("drunkenness"),
                 dplyr::matches("dui"),
                 dplyr::matches("embezzlement"),
-                dplyr::matches("family_offenses"),
+                dplyr::matches("family_off"),
                 dplyr::matches("forgery"),
                 dplyr::matches("fraud"),
 
-                dplyr::matches("total_gambling"),
-                dplyr::matches("bookmaking"),
-                dplyr::matches("number_lottery"),
-                dplyr::matches("oth_gambling"),
+                dplyr::matches("gamble_total"),
+                dplyr::matches("gamble_bookmaker"),
+                dplyr::matches("gamble_lottery"),
+                dplyr::matches("gamble_other"),
 
                 dplyr::matches("liquor"),
                 dplyr::matches("manslaught_neg"),
                 dplyr::matches("mtr_veh_theft"),
                 dplyr::matches("murder"),
-                dplyr::matches("oth_non_traffic"),
                 dplyr::matches("prostitution"),
                 dplyr::matches("rape"),
                 dplyr::matches("robbery"),
-                dplyr::matches("runaway"),
+                dplyr::matches("runaways"),
                 dplyr::matches("oth_sex_off"),
-                dplyr::matches("stolen_property"),
+                dplyr::matches("stolen_prop"),
                 dplyr::matches("suspicion"),
                 dplyr::matches("theft"),
                 dplyr::matches("vagrancy"),
                 dplyr::matches("vandalism"),
                 dplyr::matches("weapons"))
-rm(asr_simple_crimes_sex_1980_2016);
-rm(asr_simple_crimes_race_1980_2016); gc()
+rm(ucr_arrests_yearly_all_crimes_totals_sex_1974_2016); gc()
+rm(ucr_arrests_yearly_all_crimes_totals_race_1974_2016); gc()
+
+arrests <-
+  arrests %>%
+  dplyr::select(agency,
+                ORI,
+                year,
+                state,
+                population,
+                dplyr::everything())
+
 
 z = arrests[!duplicated(arrests$ORI),]
 z$temp <- paste(z$agency, z$state)
 z = z[duplicated(z$temp),]
 arrests <- arrests[!arrests$ORI %in% z$ORI, ]
 arrests$agency <- sapply(arrests$agency, simpleCap)
-arrests$state <- sapply(arrests$state, simpleCap)
-arrests$state <- gsub(" Of ", " of ", arrests$state)
+arrests$state  <- sapply(arrests$state, simpleCap)
+arrests$state  <- gsub(" Of ", " of ", arrests$state)
 
 setwd("C:/Users/user/Dropbox/R_project/crimedatatool_data/data/arrests")
 arrests <- data.table::data.table(arrests)
@@ -115,5 +135,4 @@ largest_agency <- arrests %>%
 largest_agency <- jsonlite::toJSON(largest_agency, pretty = TRUE)
 write(largest_agency, "largest_agency_choices.json")
 rm(arrests); gc()
-
 
