a85a005960d1e54c9644f5f4efd8b82d4390e820
jacobkap <jacobkap@sas.upenn.edu>
2019-05-04 23:31:09 -0400

    .

--- R/borderpatrol.R
+++ R/borderpatrol.R
@@ -2,7 +2,16 @@ source(here::here('R/utils.R'))
 setwd(here::here("data/borderpatrol"))
 
 load("C:/Users/user/Dropbox/R_project/borderpatrol/data/clean/sector_profile_2011_2017.rda")
+load("C:/Users/user/Dropbox/R_project/borderpatrol/data/clean/apprehensions_seizures_stats_2011_2017.rda")
+load("C:/Users/user/Dropbox/R_project/borderpatrol/data/clean/southwest_border_apprehensions_1960_2018.rda")
+load("C:/Users/user/Dropbox/R_project/borderpatrol/data/clean/southwest_border_deaths_1998_2018.rda")
+
+
+seizures <- apprehensions_seizures_stats_2011_2017
 border <- sector_profile_2011_2017
+save_as_csv(border, "sector_profile")
+
+save_as_csv <- function(data, file_name) {
 border$sector <- gsub(" border", " border total", border$sector)
 border$sector <- sapply(border$sector, simpleCap)
 border$sector <- gsub(" ", "_", border$sector)
@@ -13,7 +22,8 @@ for (sector in unique(border$sector)) {
     temp %>%
     dplyr::arrange(fiscal_year)
   readr::write_csv(temp,
-                   path = paste0("border_patrol_sector", "_", sector, ".csv"))
+                   path = paste0(file_name, "_", sector, ".csv"))
 
+}
 }
 
