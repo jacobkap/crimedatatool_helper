b1c9ae91c69332ddf6364495b385abb005ee2f4f
jacobkap <jacobkap@sas.upenn.edu>
2019-05-04 22:36:48 -0400

    change sector names

--- R/borderpatrol.R
+++ R/borderpatrol.R
@@ -3,11 +3,15 @@ setwd(here::here("data/borderpatrol"))
 
 load("C:/Users/user/Dropbox/R_project/borderpatrol/data/clean/sector_profile_2011_2017.rda")
 border <- sector_profile_2011_2017
+border$sector <- gsub(" border", " border total", border$sector)
+border$sector <- sapply(border$sector, simpleCap)
 border$sector <- gsub(" ", "_", border$sector)
-
 
 for (sector in unique(border$sector)) {
   temp <- border[border$sector %in% sector, ]
+  temp <-
+    temp %>%
+    dplyr::arrange(fiscal_year)
   readr::write_csv(temp,
                    path = paste0("border_patrol_sector", "_", sector, ".csv"))
 
